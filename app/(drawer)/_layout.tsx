9196903a56bd5196ee5158a4374b76b899e864c8
cielaton <vuwin24680@gmail.com>
2025-01-12 12:29:20 +0700

    [Navigation] Created a custom drawer for the drawer navigator

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,22 +1,70 @@
 import {Drawer} from "expo-router/drawer";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
+import {DrawerItem} from "@react-navigation/drawer";
+import {View} from "react-native";
+import {useState} from "react";
+
+const CustomDrawerContent = (props: any) => {
+    // Holds the focusing state of the drawer item
+    const [drawerItemState, setDrawerItemState] = useState([true, false, false, false, false]);
+
+    return <View style={{
+        flex: 1,
+        justifyContent: 'center',
+    }}>
+        <DrawerItem
+            label="Home"
+            focused={drawerItemState[0]}
+            href={"Home"}
+            onPress={() => {
+                props.navigation.navigate("Home")
+                setDrawerItemState([true, false, false, false, false]);
+            }}
+        />
+        <DrawerItem
+            label="Location"
+            focused={drawerItemState[1]}
+            onPress={() => {
+                props.navigation.navigate("Location")
+                setDrawerItemState([false, true, false, false, false]);
+            }}
+        />
+        <DrawerItem
+            label="Status"
+            focused={drawerItemState[2]}
+            onPress={() => {
+                props.navigation.navigate("Device-status")
+                setDrawerItemState([false, false, true, false, false]);
+            }}
+        />
+        <DrawerItem
+            label="Device"
+            focused={drawerItemState[3]}
+            onPress={() => {
+                props.navigation.navigate("Device-management")
+                setDrawerItemState([false, false, false, true, false]);
+            }}
+        />
+        <DrawerItem
+            label="Settings"
+            focused={drawerItemState[4]}
+            onPress={() => {
+                props.navigation.navigate("Settings")
+                setDrawerItemState([false, false, false, false, true]);
+            }}
+        />
+    </View>
+}
 
 const DrawerLayout = () => {
     return <GestureHandlerRootView style={{flex: 1}}>
-        <Drawer screenOptions={{
+        <Drawer initialRouteName={"Home"} drawerContent={CustomDrawerContent} screenOptions={{
             drawerType: 'permanent', headerShown: false, drawerStyle: {
-                width: '20%',
+                width: '15%',
             },
-            drawerItemStyle: {
-            },
-            drawerContentContainerStyle: {
-                flex: 1,
-                justifyContent: 'center',
-            }
-        }}>
-            <Drawer.Screen name="Home" options={{title: 'Home'}}/>
-            <Drawer.Screen name="Location" options={{title: 'Location'}}/>
-        </Drawer>
+            drawerItemStyle: {},
+            drawerContentContainerStyle: {}
+        }}/>
     </GestureHandlerRootView>
 }
 

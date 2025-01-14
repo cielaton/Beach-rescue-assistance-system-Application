d2547a867587a6d6e2dbac9aca8cb1ab133f3f5f
cielaton <vuwin24680@gmail.com>
2025-01-14 22:14:47 +0700

    [Status] Migrate the row item into another file and clean it up

---
+++ app/(drawer)/Status/TableRow.Status.tsx
@@ -0,0 +1,90 @@
+import {StyleSheet, Text, View} from "react-native";
+import colors from "@/constants/colors.json";
+import {Check} from "lucide-react-native";
+
+const TableRowStatus = ({id, longitude, latitude, active, uptime, boundary}: {
+    id: number,
+    longitude: number,
+    latitude: number,
+    active: boolean,
+    uptime: string,
+    boundary: string
+}) => {
+    // temporary object to hold the table row value
+    return <View style={styles.container}>
+        <View style={styles.device}>
+            <View style={styles.deviceCircle}>
+                <Text style={styles.deviceNumber}>{id}</Text>
+            </View>
+        </View>
+        <View style={styles.longitude}>
+            <Text>{longitude}</Text>
+        </View>
+        <View style={styles.latitude}>
+            <Text>{latitude}</Text>
+        </View>
+        <View style={styles.active}>
+            <View style={styles.activeCircle}>
+                <Check size={15} color={colors.latte.colors.base.hex}/>
+            </View>
+        </View>
+        <View style={styles.uptime}>
+            <Text>{uptime}</Text>
+        </View>
+        <View style={styles.boundary}>
+            <Text style={{
+                color: colors.latte.colors.green.hex
+            }}>{boundary}</Text>
+        </View>
+    </View>
+}
+
+export default TableRowStatus;
+
+const styles = StyleSheet.create({
+    container: {
+        height: 40,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    device: {
+        width: '8%',
+    },
+    deviceCircle: {
+        height: '80%',
+        aspectRatio: 1,
+        borderRadius: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(166, 227, 161, 0.5)'
+    },
+    deviceNumber: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: colors.latte.colors.green.hex
+    },
+    longitude: {
+        width: '15%',
+    },
+    latitude: {
+        width: '15%',
+    },
+    active: {
+        width: '8%',
+    },
+    activeCircle: {
+        height: '60%',
+        aspectRatio: 1,
+        borderRadius: '100%',
+        backgroundColor: colors.latte.colors.green.hex,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    uptime: {
+        width: '20%',
+    },
+    boundary: {
+        width: '10%',
+    }
+})
--- app/(drawer)/Status/index.tsx
+++ app/(drawer)/Status/index.tsx
@@ -1,8 +1,8 @@
-import {FlatList, StyleSheet, Text, View} from "react-native";
+import {FlatList, StyleSheet, View} from "react-native";
 import Heading from "@/components/Heading";
 import colors from "@/constants/colors.json";
 import TableHeaderStatus from "@/app/(drawer)/Status/TableHeader.Status";
-import {Check} from "lucide-react-native";
+import TableRowStatus from "@/app/(drawer)/Status/TableRow.Status";
 
 const data = [
     {
@@ -39,80 +39,6 @@ const data = [
     }
 ]
 
-const TableRow = ({id, longitude, latitude, active, uptime, boundary}: {
-    id: number,
-    longitude: number,
-    latitude: number,
-    active: boolean,
-    uptime: string,
-    boundary: string
-}) => {
-    // temporary object to hold the table row value
-    return <View style={{
-        height: 40,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    }}>
-        <View style={{
-            width: '8%',
-        }}>
-            <View style={{
-                height: '80%',
-                aspectRatio: 1,
-                borderRadius: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'rgba(166, 227, 161, 0.5)'
-            }}>
-                <Text style={{
-                    fontSize: 15,
-                    fontWeight: 'bold',
-                    color: colors.latte.colors.green.hex
-                }}>{id}</Text>
-            </View>
-        </View>
-        <View style={{
-            width: '15%',
-        }}>
-            <Text>{longitude}</Text>
-        </View>
-        <View style={{
-            width: '15%',
-        }}>
-            <Text>{latitude}</Text>
-        </View>
-        <View style={{
-            width: '8%',
-        }}>
-
-            <View style={
-                {
-                    height: '60%',
-                    aspectRatio: 1,
-                    borderRadius: '100%',
-                    backgroundColor: colors.latte.colors.green.hex,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
-                <Check size={15} color={colors.latte.colors.base.hex}/>
-            </View>
-        </View>
-        <View style={{
-            width: '20%',
-        }}>
-            <Text>{uptime}</Text>
-        </View>
-        <View style={{
-            width: '10%',
-        }}>
-            <Text style={{
-                color: colors.latte.colors.green.hex
-            }}>{boundary}</Text>
-        </View>
-    </View>
-}
-
 const DeviceStatusScreen = () => {
     return <View style={styles.container}>
         <View style={styles.headingWrapper}>
@@ -125,9 +51,9 @@ const DeviceStatusScreen = () => {
             <FlatList
                 data={data}
                 renderItem={({item}) =>
-                    <TableRow id={item.id} longitude={item.longitude} latitude={item.latitude}
-                              active={item.active} uptime={item.uptime}
-                              boundary={item.boundary}
+                    <TableRowStatus id={item.id} longitude={item.longitude} latitude={item.latitude}
+                                    active={item.active} uptime={item.uptime}
+                                    boundary={item.boundary}
                     />
                 }
             />

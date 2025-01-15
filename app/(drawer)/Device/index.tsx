f5a70f776bb62daeb74c905fea9ca6e8385c77ca
cielaton <vuwin24680@gmail.com>
2025-01-15 12:24:13 +0700

    [Device] Added a basic component structure for the device screen

--- app/(drawer)/Device/index.tsx
+++ app/(drawer)/Device/index.tsx
@@ -1,11 +1,42 @@
 import {StyleSheet, View} from "react-native";
 import Heading from "@/components/Heading";
 import colors from "@/constants/colors.json";
+import HorizontalSeparator from "@/components/HorizontalSeparator";
+import VerticalSeparator from "@/components/VerticalSeparator";
 
 const DeviceManagementScreen = () => {
     return <View style={styles.container}>
         <View style={styles.headingWrapper}>
             <Heading title={"Device Management"}/>
+        </View>
+        <View style={{
+            flex: 0.87,
+            flexDirection: 'row'
+        }}>
+            <View style={{
+                flex: 0.5,
+                borderRadius: 15,
+                backgroundColor: colors.latte.colors.base.hex
+            }}></View>
+            <HorizontalSeparator width={20}/>
+            <View style={{
+                flex: 0.5,
+                borderRadius: 15,
+            }}>
+                <View style={{
+                    flex: 2 / 7,
+                    borderRadius: 15,
+                    backgroundColor: colors.mocha.colors.base.hex
+                }}>
+                </View>
+                <VerticalSeparator height={20}/>
+                <View style={{
+                    flex: 5 / 7,
+                    borderRadius: 15,
+                    backgroundColor: colors.latte.colors.base.hex
+                }}>
+                </View>
+            </View>
         </View>
     </View>
 }

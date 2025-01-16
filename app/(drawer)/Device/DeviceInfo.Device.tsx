7a93716614d4c3b3767b7e4646379349babb7a59
cielaton <vuwin24680@gmail.com>
2025-01-16 14:21:31 +0700

    [Device] Refactored the DeviceInfo component

--- app/(drawer)/Device/DeviceInfo.Device.tsx
+++ app/(drawer)/Device/DeviceInfo.Device.tsx
@@ -4,19 +4,8 @@ import {BatteryFull} from "lucide-react-native";
 
 const DeviceInfoSection = ({info, value}: { info: string, value: string }) => {
     return <View>
-        <Text style={{
-            fontSize: 15,
-            lineHeight: 15,
-            fontWeight: '600',
-            color: colors.mocha.colors.text.hex
-        }}>{info}</Text>
-        <Text style={{
-            marginTop: 5,
-            fontSize: 15,
-            lineHeight: 15,
-            color: colors.mocha.colors.text.hex,
-            opacity: 0.8
-        }}>{value}</Text>
+        <Text style={styles.deviceInfo}>{info}</Text>
+        <Text style={styles.deviceInfoValue}>{value}</Text>
     </View>
 }
 
@@ -24,54 +13,15 @@ const DeviceInfo = () => {
     return <View style={styles.container}>
         <View style={styles.firstColumn}>
             <Text style={styles.heading}>Device info</Text>
-            <View style={{
-                flexDirection: 'row',
-                flex: 1,
-                alignItems: 'flex-start',
-            }}>
-                <BatteryFull style={{
-                    padding: 0,
-                    margin: 0,
-                }} size={30} color={colors.mocha.colors.green.hex}/>
-                <View style={{
-                    flex: 1,
-                    paddingLeft: 10,
-                    paddingRight: 20,
-                }}>
-                    <View style={{
-                        height: 30,
-                        justifyContent: 'center'
-                    }}>
-                        <View style={{
-                            position: "absolute",
-                            height: 15,
-                            borderRadius: 100,
-                            width: "100%",
-                            backgroundColor: colors.mocha.colors.text.hex
-                        }}/>
-                        <View style={{
-                            position: "absolute",
-                            height: 15,
-                            borderRadius: 100,
-                            width: "60%",
-                            backgroundColor: colors.mocha.colors.green.hex
-                        }}/>
+            <View style={styles.batteryContainer}>
+                <BatteryFull size={30} color={colors.mocha.colors.green.hex}/>
+                <View style={styles.batteryPercentageContainer}>
+                    <View style={styles.batteryPercentageBarWrapper}>
+                        <View style={styles.batteryPercentageBarBackground}/>
+                        <View style={styles.batteryPercentageBarProgress}/>
                     </View>
-                    <View style={{
-                        alignSelf: 'center',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        borderRadius: 6,
-                        marginTop: 10,
-                        height: 30,
-                        width: 50,
-                        backgroundColor: colors.mocha.colors.text.hex,
-                    }}>
-                        <Text style={{
-                            fontSize: 15,
-                            fontWeight: '600',
-                            color: colors.mocha.colors.base.hex
-                        }}>60%</Text>
+                    <View style={styles.batteryPercentageTextContainer}>
+                        <Text style={styles.batteryPercentageText}>60%</Text>
                     </View>
                 </View>
             </View>
@@ -97,11 +47,68 @@ const styles = StyleSheet.create({
     firstColumn: {
         flex: 2 / 3,
     },
+    batteryContainer: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'flex-start',
+    },
+    batteryPercentageContainer: {
+        flex: 1,
+        paddingLeft: 10,
+        paddingRight: 20,
+    },
+    batteryPercentageBarWrapper: {
+        height: 30,
+        justifyContent: 'center'
+    },
+    batteryPercentageBarBackground: {
+        position: "absolute",
+        height: 15,
+        borderRadius: 100,
+        width: '100%',
+        backgroundColor: colors.mocha.colors.text.hex
+    },
+    batteryPercentageBarProgress: {
+        position: "absolute",
+        height: 15,
+        borderRadius: 100,
+        width: '60%',
+        backgroundColor: colors.mocha.colors.green.hex
+    },
+    batteryPercentageTextContainer: {
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 6,
+        marginTop: 10,
+        height: 30,
+        width: 50,
+        backgroundColor: colors.mocha.colors.text.hex,
+    },
+    batteryPercentageText: {
+        fontSize: 15,
+        fontWeight: '600',
+        color: colors.mocha.colors.base.hex
+    },
+
     secondColumn: {
         flex: 1 / 3,
         paddingTop: 15,
         paddingBottom: 20,
         justifyContent: 'space-between',
+    },
+    deviceInfo: {
+        fontSize: 15,
+        lineHeight: 15,
+        fontWeight: '600',
+        color: colors.mocha.colors.text.hex
+    },
+    deviceInfoValue: {
+        marginTop: 5,
+        fontSize: 15,
+        lineHeight: 15,
+        color: colors.mocha.colors.text.hex,
+        opacity: 0.8
     },
     heading: {
         fontSize: 20,

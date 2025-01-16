e496174e40e6b804c0f34989e2d607591849b7a6
cielaton <vuwin24680@gmail.com>
2025-01-16 14:11:32 +0700

    [Device] Formed the basic structure and style of the device info section, will need to improve a bit

--- app/(drawer)/Device/DeviceInfo.Device.tsx
+++ app/(drawer)/Device/DeviceInfo.Device.tsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, View} from "react-native";
 import colors from "@/constants/colors.json";
+import {BatteryFull} from "lucide-react-native";
 
 const DeviceInfoSection = ({info, value}: { info: string, value: string }) => {
     return <View>
@@ -23,6 +24,57 @@ const DeviceInfo = () => {
     return <View style={styles.container}>
         <View style={styles.firstColumn}>
             <Text style={styles.heading}>Device info</Text>
+            <View style={{
+                flexDirection: 'row',
+                flex: 1,
+                alignItems: 'flex-start',
+            }}>
+                <BatteryFull style={{
+                    padding: 0,
+                    margin: 0,
+                }} size={30} color={colors.mocha.colors.green.hex}/>
+                <View style={{
+                    flex: 1,
+                    paddingLeft: 10,
+                    paddingRight: 20,
+                }}>
+                    <View style={{
+                        height: 30,
+                        justifyContent: 'center'
+                    }}>
+                        <View style={{
+                            position: "absolute",
+                            height: 15,
+                            borderRadius: 100,
+                            width: "100%",
+                            backgroundColor: colors.mocha.colors.text.hex
+                        }}/>
+                        <View style={{
+                            position: "absolute",
+                            height: 15,
+                            borderRadius: 100,
+                            width: "60%",
+                            backgroundColor: colors.mocha.colors.green.hex
+                        }}/>
+                    </View>
+                    <View style={{
+                        alignSelf: 'center',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: 6,
+                        marginTop: 10,
+                        height: 30,
+                        width: 50,
+                        backgroundColor: colors.mocha.colors.text.hex,
+                    }}>
+                        <Text style={{
+                            fontSize: 15,
+                            fontWeight: '600',
+                            color: colors.mocha.colors.base.hex
+                        }}>60%</Text>
+                    </View>
+                </View>
+            </View>
         </View>
         <View style={styles.secondColumn}>
             <DeviceInfoSection info={"Last update"} value={"5 minutes ago"}/>

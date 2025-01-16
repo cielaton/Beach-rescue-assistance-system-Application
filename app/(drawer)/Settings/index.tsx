b8aad9587ee44df0715cd12df03ac6ea90ff9f76
cielaton <vuwin24680@gmail.com>
2025-01-16 16:35:22 +0700

    [Settings] Start to work on Setting screen, added the heading and basic user info

--- app/(drawer)/Settings/index.tsx
+++ app/(drawer)/Settings/index.tsx
@@ -1,7 +1,31 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
+import colors from "@/constants/colors.json"
+import Heading from "@/components/Heading";
 
-const SettingsScreen= () => {
-    return <View><Text>This is the Settings screen</Text></View>
+const SettingsScreen = () => {
+    return <View style={styles.container}>
+        <View style={styles.headingWrapper}>
+            <Heading title={"Settings"}/>
+        </View>
+        <View style={styles.userInfoWrapper}>
+            <View style={styles.imageWrapper}>
+                <Image style={{height: "100%", width: "100%"}}
+                       source={require("@/assets/images/avatar.jpeg")}/>
+            </View>
+            <View style={{marginLeft: 20}}>
+                <Text style={{
+                    fontSize: 25,
+                    fontWeight: '500',
+                    color: colors.mocha.colors.base.hex
+                }}>Nguyen Van A</Text>
+                <Text style={{
+                    fontSize: 20,
+                    color: colors.mocha.colors.base.hex,
+                    opacity: 0.7
+                }}>Rescuer</Text>
+            </View>
+        </View>
+    </View>
 }
 
 export default SettingsScreen;
@@ -9,7 +33,24 @@ export default SettingsScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingHorizontal: "25%",
+        backgroundColor: colors.latte.colors.mantle.hex
+    },
+    headingWrapper: {
+        flex: 0.13,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    userInfoWrapper: {
+        flex: 0.2,
+        flexDirection: 'row',
+    },
+    imageWrapper: {
+        height: "80%",
         alignItems: 'center',
         justifyContent: 'center',
+        aspectRatio: 1,
+        overflow: 'hidden',
+        borderRadius: "100%"
     }
 })
